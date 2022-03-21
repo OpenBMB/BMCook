044bd978dbd70b247add8254805db67508ca4b88
Zhengyan Zhang <985925055@qq.com>
2022-03-21 17:28:57 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../example'))
-autodoc_mock_imports = ["cpm_kernels", "bmtrain"]
+autodoc_mock_imports = ["cpm_kernels", "bmtrain", "numpy", "tensorboard", "torch"]
 
 
 # -- Project information -----------------------------------------------------
