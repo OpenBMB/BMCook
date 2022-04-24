dd2a5ee998c02dddc0ab9837b54876c574fd7ff2
zzy <zy-z19@mails.tsinghua.edu.cn>
2022-04-24 16:55:05 +0800

    test ce

--- example/distilling/__init__.py
+++ example/distilling/__init__.py
@@ -7,11 +7,10 @@ import cpm_kernels.torch as ct
 
 class HiddenMap(bmt.DistributedModule):
     def __init__(self, dim_from, dim_to, dtype=torch.half, 
-                 init_std=0.02, int8=False):
+                 init_std=0.02):
         super().__init__()
 
         self.dtype = dtype
-        self.int8 = int8
 
         init_method = bmt.ParameterInitializer(
             nn.init.normal_, mean=0.0, std=init_std)
@@ -56,16 +55,7 @@ class BMDistill:
         student.load_state_dict(student_sd)
 
     @classmethod
-    def set_forward(cls, student, teacher, foward_fn,
-                    temp=1.0, kd_loss_scale=1.0, 
-                    output_kd_loss=False,
-                    int8=False,
-                    # Various KD loss
-                    ce_logits=False,
-                    mse_last_hidden=False,
-                    mse_hidden_states=False,
-                    mse_att=False,
-                    mse_emb=False):
+    def set_forward(cls, student, teacher, foward_fn, config):
         '''
         Modify the forward function of the student model to compute additional knowledge distillation loss.
 
@@ -77,80 +67,75 @@ class BMDistill:
         :param student: Student model.
         :param teacher: Teacher model.
         :param foward_fn: Forward function of the student model.
-        :param temp: Temperature for knowledge distillation.
-        :param kd_loss_scale: Scale of the knowledge distillation loss.
-        :param output_kd_loss: Whether to output the knowledge distillation loss.
-        :param ce_logits: Whether to use cross entropy loss for the output logits.
-        :param mse_last_hidden: Whether to use MSE loss for the last hidden state.
-        :param mse_hidden_states: Whether to use MSE loss for the hidden states.
-        :param mse_att: Whether to use MSE loss for the attention matrices.
-        :param mse_emb: Whether to use MSE loss for the embedding matrices.
+        :param config: ConfigParser object.
         :return: Modified forward function, whose return values are 1. (model, dec_input, dec_length, targets) -> loss, logits, kd_loss 2. (model, dec_input, dec_length, targets) -> loss, logits. Returns 1 if `output_kd_loss` is True, else return 2.
         '''
 
-        cls.int8 = int8
+        distill_config = config.get('distillation')
+        assert distill_config['ce_scale'] + distill_config['mse_hidn_scale'] + distill_config['mse_att_scale'] > 0, 'At least one of the distillation loss should be non-zero.'
 
-        assert any([ce_logits, mse_last_hidden, mse_hidden_states, mse_att, mse_emb])
+        if distill_config['mse_hidn_scale'] > 0:
+            # change forward function
+            pass
 
-        if mse_hidden_states:
-            cls.hidden_map = HiddenMap(teacher.dim_model, student.dim_model)
-            bmt.init_parameters(cls.hidden_map)
-            bmt.synchronize()
+            # cls.hidden_map = HiddenMap(teacher.dim_model, student.dim_model)
+            # bmt.init_parameters(cls.hidden_map)
+            # bmt.synchronize()
 
         def forward(model, dec_input, dec_length, targets, loss_func):
             outputs = foward_fn(
                 model, dec_input, dec_length, targets, loss_func)
             loss = outputs[0]
-            model_outputs = outputs[1:]
+            model_outputs = outputs[1]
             logits_s = model_outputs[0]
-            hidden_s = model_outputs[1]
-            att_scores_s = model_outputs[2]
 
-            outputs_t = teacher(dec_input, dec_length)
-            logits_t = outputs_t[0].detach()
-            hidden_t = outputs_t[1]
-            att_scores_t = outputs_t[2]
+            # hidden_s = model_outputs[1]
+            # att_scores_s = model_outputs[2]
+
+            outputs_t = teacher(dec_input, dec_length, return_logits=True)
+            logits_t = outputs_t.detach()
+
+            # hidden_t = outputs_t[1]
+            # att_scores_t = outputs_t[2]
 
             # Compute loss and d_loss
             d_loss = 0.0
-            if ce_logits:
+            if distill_config['ce_scale'] > 0:
+                temp = distill_config['ce_temp']
                 prob_t = F.softmax(logits_t / temp, dim=-1)
                 log_prob_s = F.log_softmax(logits_s / temp, dim=-1)
-                d_loss += -(prob_t * log_prob_s).sum(dim=1).mean()
+                d_loss += -(prob_t * log_prob_s).sum(dim=1).mean() * distill_config['ce_scale']
 
-            # MSE loss on last hidden states
-            if mse_last_hidden:
-                h_t = hidden_t[-1].detach()
-                h_s = hidden_s[-1]
-                d_loss += F.mse_loss(h_s, h_t)
+                bmt.print_rank(prob_t, log_prob_s)
+                exit()
 
             # MSE loss on all hidden states
-            if mse_hidden_states:
-                cls.hidden_map.to(dec_input.device)
-                ratio = len(hidden_t) // len(hidden_s)
-                fit_target = [hidden_t[i]
-                    for i in range(ratio - 1, len(hidden_t), ratio)]
-                for h_s, h_t in zip(hidden_s, fit_target):
-                    h_t = h_t.detach()
-                    # Map hidden states from student to teacher
-                    h_s = cls.hidden_map(h_s)
-                    d_loss += F.mse_loss(h_s, h_t)
+            # if mse_hidden_states:
+            #     cls.hidden_map.to(dec_input.device)
+            #     ratio = len(hidden_t) // len(hidden_s)
+            #     fit_target = [hidden_t[i]
+            #         for i in range(ratio - 1, len(hidden_t), ratio)]
+            #     for h_s, h_t in zip(hidden_s, fit_target):
+            #         h_t = h_t.detach()
+            #         # Map hidden states from student to teacher
+            #         h_s = cls.hidden_map(h_s)
+            #         d_loss += F.mse_loss(h_s, h_t)
 
             # MSE loss on attention scores
-            if mse_att:
-                ratio = len(hidden_t) // len(hidden_s)
-                fit_target = [att_scores_t[i]
-                    for i in range(ratio - 1, len(att_scores_t), ratio)]
-                for att_s, att_t in zip(att_scores_s, fit_target):
-                    att_t = att_t.detach()
-                    att_t = att_t[att_t != -torch.inf]
-                    att_s = att_s[att_s != -torch.inf]
-                    d_loss += F.mse_loss(att_s, att_t)
+            # if mse_att:
+            #     ratio = len(hidden_t) // len(hidden_s)
+            #     fit_target = [att_scores_t[i]
+            #         for i in range(ratio - 1, len(att_scores_t), ratio)]
+            #     for att_s, att_t in zip(att_scores_s, fit_target):
+            #         att_t = att_t.detach()
+            #         att_t = att_t[att_t != -torch.inf]
+            #         att_s = att_s[att_s != -torch.inf]
+            #         d_loss += F.mse_loss(att_s, att_t)
 
-            loss = loss + kd_loss_scale * d_loss
+            loss = loss + d_loss
 
-            outputs = (loss, logits_s)
-            if output_kd_loss:
-                outputs = outputs + (d_loss, )
+            # update loss & append distillation loss
+            outputs[0] = loss
+            outputs = outputs + [d_loss, ]
             return outputs
         return forward
---
+++ example/train_new.py
@@ -0,0 +1,227 @@
+import json
+import torch
+import random
+import bmtrain as bmt
+import layers
+from tqdm import tqdm
+import time
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../'))
+
+from data import MMapIndexedDataset, Dataset
+import numpy as np
+import pickle as pkl
+from pruning import BMPrune, m4n2_2d_greedy
+from model_center.model import GPT2Config, GPT2
+from distilling import BMDistill
+from arguments import parse_args
+from pathlib import Path
+import os
+import json
+
+import os
+from arguments import parse_args
+from pathlib import Path
+
+from utils.config import ConfigParser
+
+def print_inspect(model, name):
+    bmt.print_rank(
+        bmt.inspect.format_summary(
+            bmt.inspect.inspect_model(model, name)
+        )
+    )
+
+class Trainer:
+    @staticmethod
+    def batch_iter_shuf(dataset : Dataset, batch_size, rank, world_size):
+        st = 0
+        end = len(dataset)
+
+        local_size = len(dataset) // world_size
+        idx = list(range(local_size))
+        random.shuffle(idx)
+
+        batch = []
+        while st < local_size:
+            it = dataset[idx[st]*world_size + rank]
+            if it is not None:
+                batch.append( it )
+            st += 1
+            if len(batch) == batch_size:
+                yield {
+                    "ctx": torch.stack([it["ctx"] for it in batch]),
+                    "len_ctx": torch.LongTensor([it["len_ctx"] for it in batch]),
+                    'target': torch.stack([it["target"] for it in batch]),
+                }
+                batch = []
+
+    @staticmethod
+    def batch_iter(dataset : Dataset, batch_size, rank, world_size):
+        st = 0
+        end = len(dataset)
+        batch = []
+        while st < end:
+            it = dataset[st + rank]
+            if it is not None:
+                batch.append( it )
+            st += world_size
+            if len(batch) == batch_size:
+                yield {
+                    "ctx": torch.stack([it["ctx"] for it in batch]),
+                    "len_ctx": torch.LongTensor([it["len_ctx"] for it in batch]),
+                    'target': torch.stack([it["target"] for it in batch]),
+                }
+                batch = []
+
+    @staticmethod
+    def forward(model, dec_input, dec_length, targets, loss_func):
+        outputs = model(
+            dec_input, dec_length, return_logits=True)
+        logits = outputs
+        batch, seq_len, vocab_out_size = logits.size()
+
+        loss = loss_func(logits.view(batch * seq_len, vocab_out_size), targets.view(batch * seq_len))
+
+        return [loss, logits]
+
+
+def main():
+    bmt.init_distributed()
+
+    # args = parse_args()
+    # save_dir = Path(args.save_dir)
+    # ckpt_dir = save_dir / 'checkpoints'
+    # os.makedirs(ckpt_dir, exist_ok=True)
+    # json.dump(vars(args), open(save_dir / 'train_args.json', 'w'), indent=2)
+
+    # model = get_model(args.model, args.init_std)
+    # bmt.init_parameters(model)
+
+    gpt = GPT2.from_pretrained("gpt2-base")
+
+    config = ConfigParser('/home/zhangzhengyan/BMCook/example/configs/test.json')
+    Trainer.forward = BMDistill.set_forward(gpt, gpt, Trainer.forward, config)
+
+    # Distillation
+    # if args.use_kd:
+    #     #assert args.model == "gpt-j", "Currently only support KD with GPT-J"
+    #     teacher = GPTJ(
+    #         num_dec=28,
+    #         dim_model=4096, num_heads=16, dim_head=256, dim_ff=16384,
+    #         vocab_size=50400,
+    #         init_std=args.init_std,
+    #         position_bias_num_buckets=32, position_bias_max_distance=128,
+    #         eps=1e-6, int8=False, dtype=torch.half
+    #     )
+    #     bmt.init_parameters(teacher)
+    #     bmt.load(teacher, args.load_teacher)
+
+    #     Trainer.forward = BMDistill.set_forward(
+    #         model,
+    #         teacher,
+    #         Trainer.forward,
+    #         output_kd_loss=True,
+    #         temp=args.kd_temp,
+    #         kd_loss_scale=args.kd_loss_scale,
+    #         ce_logits=args.kd_ce_logits,
+    #         mse_last_hidden=args.kd_mse_last_hidden,
+    #         mse_hidden_states=args.kd_mse_hidn,
+    #         mse_att=args.kd_mse_att,
+    #         mse_emb=args.kd_mse_emb,
+    #     )
+    #     teacher.eval()
+
+    #print_inspect(model, "*")
+    #bmt.print_rank("Model mem\n", torch.cuda.memory_summary())
+    bmt.synchronize()
+
+    batch_size = 2
+    seq_len = 32
+
+    for i in range(bmt.world_size()):
+        sent = torch.randint(0, 10240, (batch_size, seq_len + 1))
+        enc_length = torch.randint(16, seq_len, (batch_size,)).long().cuda()
+        enc_input = sent[:, :-1].long().cuda()
+        targets = sent[:, 1:].long().cuda()
+        mask = torch.arange(seq_len).long().cuda()[None, :] < enc_length[:, None]
+        targets = torch.where(
+            mask,
+            targets,
+            torch.full_like(targets, -100, dtype=torch.long)
+        )
+
+        if i == bmt.rank():
+            break
+
+    loss_func = torch.nn.CrossEntropyLoss(ignore_index=-100)
+    optimizer = bmt.optim.AdamOptimizer(gpt.parameters(), scale=2**20)
+    lr_scheduler = bmt.lr_scheduler.Noam(optimizer, start_lr=1e-4, warmup_iter=2000, end_iter=100000)
+
+    # for pruning
+    # if args.use_pruning:
+    #     BMPrune.compute_mask(model, m4n2_2d_greedy, checkpoint=args.pruning_mask_path)
+    #     BMPrune.set_optim_for_pruning(optimizer)
+
+    # if args.moe:
+    #     from moe import BMMoE
+    #     BMMoE.moefy(model, args.num_expert, args.topk, checkpoint=args.moe_ckpt)
+
+    bmt.synchronize()
+
+    # dataset = Dataset(
+    #     MMapIndexedDataset("/data/home/scv0540/zzy/openwebtxt/openwebtext_text_document"),
+    #     dec_len
+    # )
+
+    # if args.eval:
+    #     eval_losses = []
+
+    # if args.save_hidden:
+    #     os.makedirs(save_dir / 'hiddens', exist_ok=True)
+    #     model.eval()
+
+    #     for iteration, data in enumerate(Trainer.batch_iter(dataset, batch_size, bmt.rank(), bmt.world_size())):
+
+    #         if iteration == 100:
+    #             break
+
+    #         with bmt.inspect.inspect_tensor() as inspector:
+
+    #             dec_input = data["ctx"].int()
+    #             dec_length = data["len_ctx"].int()
+    #             dec_mask = torch.arange(dec_len)[None, :].repeat(batch_size, 1) < dec_length[:, None]
+    #             targets = torch.where(dec_mask, data["target"].long(), torch.scalar_tensor(-100, dtype=torch.long))
+
+    #             targets = targets.cuda()
+    #             dec_input = dec_input.cuda()
+    #             dec_length = dec_length.cuda()
+                
+    #             with torch.no_grad():
+    #                 loss, logits, _, _ = Trainer.forward(model, dec_input, dec_length, targets, loss_func)
+            
+    #         tensors = [x['tensor'] for x in inspector._summary if 'ff_x' in x['name']]
+               
+    #         torch.save(tensors, save_dir / 'hiddens' / '{}_{}'.format(iteration, bmt.rank()) )
+    #         bmt.print_rank("Iteration:", iteration)
+    #     exit()
+
+    for iteration in range(1000):
+        optimizer.zero_grad()
+
+        outputs = Trainer.forward(
+            gpt, enc_input, enc_length, targets, loss_func)
+
+        loss = optimizer.loss_scale(outputs[0])
+
+        loss.backward()
+        bmt.optim_step(optimizer, lr_scheduler)
+
+        bmt.print_rank("Iteration:", iteration, "Loss:", loss.item())
+        
+
+
+if __name__ == '__main__': 
+    main()
